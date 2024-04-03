85f7d72262bf4039224e75707d85eb77652000d0
ilacraftYT <rubencitoperez569@gmail.com>
2024-04-03 00:15:38 -0600

    🚧 Add /updateDashboard endpoint
    untested

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,95 @@
 import Fastify, { type FastifyRequest } from "fastify"
 import redis from "@fastify/redis"
 import mongodb from "@fastify/mongodb"
-import type { DashboardDocument } from "./types"
+import type { DashboardDocument, UpdateDashboardBody } from "./types"
 
 const fastify = Fastify({
-  logger: true
+  logger: true,
 })
 
 fastify.register(redis, {
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT as unknown as number,
   password: process.env.REDIS_PASS,
-  closeClient: true
+  closeClient: true,
 })
 
 fastify.register(mongodb, {
   url: process.env.MONGO_URL,
-  forceClose: true
+  forceClose: true,
 })
 
-fastify.post("/updateServers", async (req: FastifyRequest<{ Body: string[] | unknown }>, res) => {
-  if (!Array.isArray(req.body) || (req.body.length == 1 && typeof req.body[0] == typeof "")) {
-    res.status(400)
-    return { message: "Invalid body." }
+fastify.post(
+  "/updateServers",
+  async (req: FastifyRequest<{ Body: string[] | unknown }>, res) => {
+    if (
+      !Array.isArray(req.body) ||
+      (req.body.length == 1 && typeof req.body[0] == typeof "")
+    ) {
+      res.status(400)
+      return { message: "Invalid body." }
+    }
+
+    if (!req.headers.authorization?.startsWith("Basic")) {
+      res.status(400)
+      return { message: "Authorization method not supported." }
+    }
+
+    const authHeader = req.headers.authorization?.split(" ")[1]
+    if (!authHeader) {
+      res.status(401)
+      return { message: "Authorization header not provided." }
+    }
+
+    const [botId, authKey] = Buffer.from(authHeader, "base64")
+      .toString()
+      .split(":")
+    if (!botId || !authKey) {
+      res.status(401)
+      return { message: "Bot ID or authorization key unspecified." }
+    }
+
+    const botData = await fastify.mongo.client
+      .db("dashio")
+      .collection<DashboardDocument>("dashboards")
+      .findOne({ _id: botId })
+
+    if (!botData || authKey !== botData.auth_key) {
+      res.status(401)
+      return { message: "Invalid bot ID or authorization key." }
+    }
+
+    fastify.redis.sadd(botId, req.body)
+    return { message: "OK" }
   }
+)
 
-  if (!req.headers.authorization?.startsWith("Basic")) {
-    res.status(400)
-    return { message: "Authorization method not supported." }
+fastify.post(
+  "/private/updateDashboard",
+  async (req: FastifyRequest<{ Body: UpdateDashboardBody }>, res) => {
+    if (req.headers.authorization?.split(" ")[1] != process.env.FRONT_SECRET) {
+      res.status(401)
+      return { message: "Incorrect authorization key." }
+    }
+
+    const dash = await fastify.mongo.client
+      .db("dashio")
+      .collection<DashboardDocument>("dashboards")
+      .updateOne(
+        { _id: req.body.id },
+        {
+          data: JSON.stringify(req.body.data),
+        }
+      )
+
+    if (!dash) return { message: "Could not find specified record." }
+
+    return {
+      message: "OK"
+    }
   }
-
-  const authHeader = req.headers.authorization.split(" ")[1]
-  if (!authHeader) {
-    res.status(401)
-    return { message: "Authorization header not provided." }
-  }
-
-  const [botId, authKey] = Buffer.from(authHeader, "base64").toString().split(":")
-  if (!botId || !authKey) {
-    res.status(401)
-    return { message: "Bot ID or authorization key unspecified." }
-  }
-
-  const botData = await fastify.mongo.client
-    .db("dashio")
-    .collection<DashboardDocument>("dashboards")
-    .findOne({ _id: botId })
-
-  if (!botData || authKey !== botData.auth_key) {
-    res.status(401)
-    return { message: "Invalid bot ID or authorization key." }
-  }
-
-  fastify.redis.sadd(botId, req.body)
-  return { message: "OK" }
-})
-
+)
 
 fastify.listen({
-  port: 3000
+  port: 3000,
 })
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,36 @@ export interface DashboardDocument {
   auth_key: string;
   premium: boolean;
 }
+
+export interface UpdateDashboardBody {
+  id: string;
+  data: {
+    pages: DashboardPage[]
+  }
+}
+
+export interface DashboardPage {
+  display_name: string;
+  options: DashboardOption[]
+}
+
+export interface DashboardOption {
+  id: string;
+  display_name: string;
+  return_type: boolean | string | string[] | number;
+  limits: Partial<{
+    /**
+     * For text based inputs, it means the maximum characters allowed.
+     * For multiple-select inputs, the maximum of items allowed.
+     * Means the maximum value available in the slider.
+     */
+    max: number;
+    /**
+     * For text based inputs, it means the minimum characters allowed.
+     * For multiple-select inputs, the minimum of items allowed.
+     * Means the minimum value available in the slider.
+     */
+    min: number;
+  }>;
+  name: "boolean" | "input" | "textarea" | "multirow" | "slider";
+}
